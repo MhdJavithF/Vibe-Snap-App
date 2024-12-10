5f27e78d928fe6ae4c04b9cfe9f24ea36479b6c1
MhdJavithF <javithmd001@gmail.com>
2024-12-11 02:57:40 +0530

    firebase upadted and tried for existing users to fetch from db

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,19 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider } from "firebase/auth";
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcWaYWpdByNNsKVS7fgxhn4oyhpBDIJLQ",
-  authDomain: "vibe-snap.firebaseapp.com",
-  projectId: "vibe-snap",
-  storageBucket: "vibe-snap.firebasestorage.app",
-  messagingSenderId: "944641652348",
-  appId: "1:944641652348:web:1fe573b54caebe250e7bf9",
-  measurementId: "G-BFE7315EDW"
+  apiKey: "AIzaSyCZeQiwwb6mrw1J3tBlWuJMc_TdjZJ4Kb8",
+  authDomain: "vibe-snap-76265.firebaseapp.com",
+  projectId: "vibe-snap-76265",
+  storageBucket: "vibe-snap-76265.firebasestorage.app",
+  messagingSenderId: "730556325624",
+  appId: "1:730556325624:web:273f52e225aa71af92a50d"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-export {auth, provider};
+export {auth, provider, db};
