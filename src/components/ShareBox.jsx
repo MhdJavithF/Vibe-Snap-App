5efe0187578a00ea70772770f8f391e5a5dd581b
MhdJavithF <javithmd001@gmail.com>
2024-12-12 01:00:37 +0530

    share box

--- src/components/ShareBox.jsx
+++ src/components/ShareBox.jsx
@@ -1,27 +1,67 @@
-import React from 'react'
+import {
+  WhatsappShareButton,
+  FacebookShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+} from "react-share";
+import { FaTwitter, FaFacebook, FaReddit, FaTelegram, FaWhatsapp, FaInstagram, FaDiscord, FaFacebookMessenger } from "react-icons/fa";
+import "../styles/share.css"
 
 const ShareBox = () => {
+  const shareUrl = "https://example.com"; // Replace with your URL
+  const shareTitle = "Check out this post!";
+
   return (
-    <div className='share-wrapper'>
-      <div className='share-head'>
+    <div className="share-wrapper">
+      <div className="share-head">
         <p>Share Post</p>
-        <div className='close'>X</div>
+        <div className="close">X</div>
       </div>
 
       <div className="share-icons">
-        <div className="icon">twitter</div>
-        <div className="icon">FB</div>
-        <div className="icon">Red</div>
-        <div className="icon">Dic</div>
-        <div className="icon">WP</div>
-        <div className="icon">Msgr</div>
-        <div className="icon">Tele</div>
-        <div className="icon">Inst</div>
+        <div className="icon">
+          <TwitterShareButton url={shareUrl} title={shareTitle}>
+            <FaTwitter />
+          </TwitterShareButton>
+        </div>
+        <div className="icon">
+          <FacebookShareButton url={shareUrl} quote={shareTitle}>
+            <FaFacebook />
+          </FacebookShareButton>
+        </div>
+        <div className="icon">
+          <RedditShareButton url={shareUrl} title={shareTitle}>
+            <FaReddit />
+          </RedditShareButton>
+        </div>
+        <div className="icon">
+          <FaDiscord/>
+        </div>
+        <div className="icon">
+          <WhatsappShareButton url={shareUrl} subject={shareTitle} body="Check this out!">
+            <FaWhatsapp />
+          </WhatsappShareButton>
+        </div>
+        <div className="icon">
+          <FaFacebookMessenger/>
+        </div>
+        <div className="icon">
+          <TelegramShareButton url={shareUrl} title={shareTitle}>
+            <FaTelegram />
+          </TelegramShareButton>
+        </div>
+        <div className="icon">
+          <FaInstagram/>
+        </div>
       </div>
 
-      <div className="page-link"></div>
+      <div className="page-link">
+        <p>Page Link</p>
+        <div>copy</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShareBox
+export default ShareBox;
